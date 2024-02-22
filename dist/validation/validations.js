0479745a0b184b8dc8f16a2ada95a4bbd7bbd9ca
latifah <kimtifah2@gmail.com>
2024-02-22 23:14:06 +0200

    fixed typo on validations

--- dist/validation/validations.js
+++ dist/validation/validations.js
@@ -4,32 +4,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.messageValidations = exports.loginValidations = exports.blogValidations = void 0;
-const joi_1 = __importDefault(require("joi"));
+const Joi_1 = __importDefault(require("joi"));
 //blog validations
 const blogValidations = (blogs) => {
-    const blogSchema = joi_1.default.object({
-        title: joi_1.default.string().required().min(5).max(20),
-        description: joi_1.default.string().required().min(10).max(100),
-        photo: joi_1.default.string().required()
+    const blogSchema = Joi_1.default.object({
+        title: Joi_1.default.string().required().min(5).max(20),
+        description: Joi_1.default.string().required().min(10).max(100),
+        photo: Joi_1.default.string().required()
     });
     return blogSchema.validate(blogs);
 };
 exports.blogValidations = blogValidations;
 //login validation
 const loginValidations = (login) => {
-    const loginSchema = joi_1.default.object({
-        email: joi_1.default.string().required().email(),
-        password: joi_1.default.string().required().min(5).max(10),
+    const loginSchema = Joi_1.default.object({
+        email: Joi_1.default.string().required().email(),
+        password: Joi_1.default.string().required().min(5).max(10),
     });
     return loginSchema.validate(login);
 };
 exports.loginValidations = loginValidations;
 //
 const messageValidations = (messages) => {
-    const messageSchema = joi_1.default.object({
-        name: joi_1.default.string().required().min(5).max(20),
-        email: joi_1.default.string().required().email(),
-        message: joi_1.default.string().required(),
+    const messageSchema = Joi_1.default.object({
+        name: Joi_1.default.string().required().min(5).max(20),
+        email: Joi_1.default.string().required().email(),
+        message: Joi_1.default.string().required(),
     });
     return messageSchema.validate(messages);
 };
